886c7773809d358eab51b1a32f2dae06dd7b7fff
Luisa Lamprea <Andromeda24@users.noreply.github.com>
2026-06-19 20:55:00 -0500

    text filter
    Include text filters in the Explorer Catalog service

--- backend/app/routes/activities_router.py
+++ backend/app/routes/activities_router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from ..services.openai_service import generate_text
 from typing import Optional
 from ..services.activity_catalog import getActivitiesCatalog
 
@@ -8,12 +7,12 @@ from ..services.activity_catalog import getActivitiesCatalog
 router = APIRouter()
 
 @router.get("")
-async def get_activityList(filter: Optional[str] = "", lang: Optional[str] = "en"):
+async def get_activityList(lang: Optional[str] = "en"):
     #return get__filtered_activities(0,filter,lang)
-    return await getActivitiesCatalog(0)
+    return await getActivitiesCatalog(0,"")
 
 
 @router.get("/{level}")
 async def get__filtered_activities(level: int, filter: Optional[str] = "", lang: Optional[str] = "en"):
-    return await getActivitiesCatalog(level)
+    return await getActivitiesCatalog(level,filter)
 
--- backend/app/services/activity_catalog.py
+++ backend/app/services/activity_catalog.py
@@ -7,10 +7,12 @@ import json
 
 
 class Activity(BaseModel):
+    id: int
     name: str
     description: str
     weekday: str
     time: str
+    match:str
 
 class ActivitiesResponse(BaseModel):
     level: int
@@ -27,33 +29,49 @@ filesearchtool = FileSearchTool(
 agent = Agent(
     name="Extracurricular Activities Explorer",
     instructions=(
-        "You are an AI agent that reads a vector store and filters activities by the provided level. "
+        "You are an AI agent that reads a vector store and filters activities by the provided level and text. "
         "Each activity is offered for a range of levels (grades in a K-12 school) named 1,2,3,...,11,12. "
         "If level = 0, return all the activities. "
+        "If filter is empty, return all the activities. "
+        "Use this definition: One activity matches one sentence if one of this conditions is true: "
+        " - te sentence asks for a name, and the activity contains at least one of the words"
+        " - the sentence asks for gender, and the activity include explicitly that gender in the name or the description, or do not talk about gender explicitly"
+        " - the sentence include a word similar to the category"
+        " - the sentence is related with the description "
         "You MUST return ONLY valid JSON matching this schema:\n\n"
         "{\n"
         '  "level": <int>,\n'
         '  "activities": [\n'
         "    {\n"
+        '      "id": <int>,\n'
         '      "name": "<string>",\n'
         '      "description": "<string>",\n'
         '      "weekday": "<string>",\n'
-        '      "time": "<string>"\n'
+        '      "time": "<string>",\n'
+        '      "match":"<string>",\n'
         "    }\n"
         "  ]\n"
         "}\n\n"
+        " if you don't have any of the fields required, fill it with 0 if the field type is int or the empty string if the field type is str"
         "Use in the description exactly the same words used in the source. Use only the information available in he tool"
+        "match contains the rule or rules that matched the activity. Include each activity only once"
         "Do not return explanations. Do not return text outside JSON."
     ),
     tools=[filesearchtool]
 )
 
 
-async def getActivitiesCatalog(level: int):
+async def getActivitiesCatalog(level: int, filter: str):
+    
+    prompt = f"Get activities for level {level}"
+    if filter: 
+        prompt += f" and whose data matches the sentence: '{filter}'" 
+    print (prompt)
+
     result = await Runner.run(agent, input=[
         {
             "role": "user",
-            "content": f"Get activities for level {level}"
+            "content": prompt
         }
     ])
 
